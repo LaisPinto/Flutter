6b949e6ff77522585171cf8669830accd4dcdd4f
LaisPinto <44181586+LaisPinto@users.noreply.github.com>
2023-08-04 13:49:53 -0300

    Delete pokemon_app directory

--- pokemon_app/lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'pages/_export_pages.dart';
-
-void main() => runApp(const MyApp());
--- pokemon_app/lib/pages/_export_pages.dart
+++
@@ -1,3 +0,0 @@
-export 'my_app.dart';
-export 'my_home_page.dart';
-export 'pokemon_detail_screen.dart';
